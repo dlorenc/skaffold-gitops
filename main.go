aca1e5cf7b05fd841ac03324b08c43c04569b0e3
dlorenc <lorenc.d@gmail.com>
2018-10-25 03:43:29 -0700

    gitops

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Hello JenkinsWorld Nice, you've requested: %s\n", r.URL.Path)
+                fmt.Fprintf(w, "Hello JenkinsWorld Nice gitops, you've requested: %s\n", r.URL.Path)
         })
         fmt.Println("Listening on port 8000")
         http.ListenAndServe(":8000", nil)
