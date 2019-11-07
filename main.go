954cc1f72fd51efa7c42dae2a2bf5aa9c2e56373
Dan Lorenc <lorenc.d@gmail.com>
2019-11-07 10:53:39 -0600

    Change  main.go

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
+
 import (
-        "fmt"
-        "net/http"
+	"fmt"
+	"net/http"
 )
+
 func main() {
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Hello JenkinsWorld Nice gitops, you've requested: %s\n", r.URL.Path)
-        })
-        fmt.Println("Listening on port 8000")
-        http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello Allhands, you've requested: %s\n", r.URL.Path)
+	})
+	fmt.Println("Listening on port 8000")
+	http.ListenAndServe(":8000", nil)
 }
